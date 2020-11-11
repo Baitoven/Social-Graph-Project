2f60daf2945b65a5038bb1f8e92e9673b2901dfc
Baitoven <adriencochard333@gmail.com>
2020-11-11 11:47:27 +0100

    added JSON queries and material ui support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+/* eslint-disable import/no-commonjs */
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
 
-// You can delete this file if you're not using it
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPlugin({ name: '@babel/plugin-proposal-decorators', options: { legacy: true } })
+  actions.setBabelPlugin({ name: '@babel/plugin-proposal-optional-chaining', options: {} })
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+        allFile {
+            edges {
+                node {
+                    id
+                    name
+                }
+            }
+        }
+    }
+  `) 
+}
